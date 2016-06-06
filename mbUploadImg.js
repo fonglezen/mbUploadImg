411fd3213afecd7c360f88bf0cca90e0012fa64a
echofong <1176584225@qq.com>
2016-06-06 14:15:19 +0800

    添加了改变图片大小的方法
    同时修改了代码的结构

---
+++ mbUploadImg.js
@@ -0,0 +1,105 @@
+
+	var mbUploadImg = {
+		changeSize:false,
+		maxWidth:2000,
+		maxHeight:1500,
+		imgurl:null,
+		
+
+		init:function(obj){
+			var _this = this;
+			for(var v in obj){
+				this[v] = obj[v];
+			}
+
+		},
+
+		imgHandle:function(src){
+
+		},
+
+		handleFile:function(){
+
+			var _this = mbUploadImg;
+
+			window.URL = window.URL || window.webkitURL;
+			var sUserAgent= navigator.userAgent.toLowerCase();
+			var selected_file = upload.files[0];
+			//判断文件类型是否为图片
+			var imageType = /image.*/;
+    
+		    if (!selected_file.type.match(imageType)) {
+		      return false;
+		    }
+		   
+
+			if(sUserAgent.match(/android/i) == "android"){
+		    	
+			    var reader = new FileReader();
+			    reader.onload = function(e) {
+			    	_this.imgurl = e.target.result;
+			    	console.log('_this.changeSize = ' + _this.changeSize)
+			    	if(_this.changeSize){
+			    		_this.changeSizeHandle(_this.imgurl);
+			    	}else{
+			    		_this.imgHandle(_this.imgurl);
+			    	}
+			    };
+			    reader.readAsDataURL(selected_file);
+
+			}else{
+				// console.log('xxx')
+			    var reader = new FileReader();
+			    reader.onload = function(e) {
+			    	_this.imgurl = e.target.result;
+			    	console.log('_this.changeSize = ' + _this.changeSize)
+			    	if(_this.changeSize){
+			    		_this.changeSizeHandle(_this.imgurl);
+			    	}else{
+			    		_this.imgHandle(_this.imgurl);
+			    	} 
+			    };
+			    reader.readAsDataURL(selected_file);
+			}
+		},
+
+		changeSizeHandle:function(src){
+			var _t = this;
+			var img = new Image();
+			var newSrc = '';
+			var qlt = 1;
+
+			img.onload = function(){
+				var iWidth = img.width;
+				var iHeight = img.height;
+				var nWidth = iWidth , nHeight = iHeight;
+				if(iWidth > iHeight){
+					//宽型图片
+					if(iWidth > _t.maxWidth){
+						nWidth = _t.maxWidth;
+						nHeight = (iHeight/iWidth)*nWidth;
+						qlt = _t.maxWidth / iWidth;
+					}
+				}else if(iHeight > iWidth){
+					//高型图片
+					if(iHeight > _t.maxHeight){
+						nHeight = _t.maxHeight;
+						nWidth = (iWidth/iHeight)*nHeight;
+						qlt = _t.maxHeight / iHeight;
+					}
+				}
+				// console.log(img)
+				var can = document.createElement('canvas');
+				var ctx = can.getContext('2d');
+				can.width = nWidth;
+				can.height = nHeight;
+				ctx.drawImage(img,0,0,nWidth,nHeight);
+				
+				newSrc = can.toDataURL("image/jpeg",qlt);
+				_t.imgHandle(newSrc);
+
+			}
+			img.src = src;
+		}
+	}
+
